c20be10e0d696560b583497c2f43b65d0b04f770
G484692024 <248469@st.takushoku-u.ac.jp>
2025-06-04 23:28:21 +0900

    index.html更新とプロジェクト課題の完成

--- weather.js
+++ weather.js
@@ -1,40 +1,89 @@
-
 // 課題3-2 のプログラムはこの関数の中に記述すること
 function print() {
-  console.log(data);
-
+  console.log(data.id);
 }
+
 // 課題5-1 の関数 printDom() はここに記述すること
 function printDom(data) {
+  let m = document.querySelector('div#result');
+  if(m!==null){
+    m.remove();
+  }
   let l = document.createElement('div');
   l.setAttribute('id', 'result');
   let u = document.querySelector('body');
   u.insertAdjacentElement('beforeend', l);
-  let s = document.createElement('p');
-	s.textContent = (data.id);
-	let t = document.querySelector('body');
-	t.insertAdjacentElement('afterend', s);
-}
-let b = document.querySelector('div#result');
-b.addEventListener('click', printDom);
-// 課題5-1 のイベントハンドラの定義
-function show() {
+
+  let s = document.createElement('div');
+	s.textContent = "経度: "+(data.coord.lon);
+	l.insertAdjacentElement('beforeend', s);
   
+  s = document.createElement('div');
+  s.textContent = "緯度: "+(data.coord.lat);
+	l.insertAdjacentElement('beforeend', s);
+
+  s = document.createElement('div');
+  s.textContent = "天気: "+(data.weather[0].description);
+	l.insertAdjacentElement('beforeend', s);
+
+  s = document.createElement('div');
+  s.textContent = "最低気温: "+(data.main.temp_min);
+	l.insertAdjacentElement('beforeend', s);
+
+  s = document.createElement('div');
+  s.textContent = "最高気温: "+(data.main.temp_max);
+	l.insertAdjacentElement('beforeend', s);
+
+  s = document.createElement('div');
+  s.textContent = "湿度: "+(data.main.humidity);
+	l.insertAdjacentElement('beforeend', s);
+ 
+  s = document.createElement('div');
+  s.textContent = "風速: "+(data.wind.speed);
+	l.insertAdjacentElement('beforeend', s);
+
+  s = document.createElement('div');
+  s.textContent = "風向: "+(data.wind.deg);
+	l.insertAdjacentElement('beforeend', s); 
 }
 
-// 課題5-1, 6-1 のイベントハンドラ登録処理は以下に記述
-
-
-
+// 課題6-1 のイベントハンドラ登録処理は以下に記述
+let b = document.querySelector('button#sendRequest');
+b.addEventListener('click', sendRequest);
 
 // 課題6-1 のイベントハンドラ sendRequest() の定義
 function sendRequest() {
-
+  let k = document.querySelector('select#key');
+	let idx = k.selectedIndex;
+	let os = k.querySelectorAll('option');
+	let o = os.item(idx);
+  if(idx!==0){
+    let kekka = o.textContent+'の検索結果';
+    let p = document.querySelector('p');
+    p.textContent = kekka;
+    let url = 'https://www.nishita-lab.org/web-contents/jsons/openweather/'+o.getAttribute('value')+'.json';
+    axios.get(url)
+          .then(showResult)
+          .catch(showError)
+          .then(finish);
+  }
 }
 
 // 課題6-1: 通信が成功した時の処理は以下に記述
 function showResult(resp) {
+  let data = resp.data;
 
+    // data が文字列型なら，オブジェクトに変換する
+    if (typeof data === 'string') {
+        data = JSON.parse(data);
+    }
+
+    // data をコンソールに出力
+    console.log(data);
+
+    // data.x を出力
+    console.log(data.x);
+    printDom(data);
 }
 
 // 課題6-1: 通信エラーが発生した時の処理
@@ -46,55 +95,3 @@ function showError(err) {
 function finish() {
     console.log('Ajax 通信が終わりました');
 }
-
-////////////////////////////////////////
-// 以下はグルメのデータサンプル
-// 注意: 第5回までは以下を変更しないこと！
-// 注意2: 課題6-1 で以下をすべて削除すること
-let data = {
-  "coord": {
-    "lon": 116.3972,
-    "lat": 39.9075
-  },
-  "weather": [
-    {
-      "id": 803,
-      "main": "Clouds",
-      "description": "曇りがち",
-      "icon": "04d"
-    }
-  ],
-  "base": "stations",
-  "main": {
-    "temp": 9.94,
-    "feels_like": 8.65,
-    "temp_min": 9.94,
-    "temp_max": 9.94,
-    "pressure": 1022,
-    "humidity": 14,
-    "sea_level": 1022,
-    "grnd_level": 1016
-  },
-  "visibility": 10000,
-  "wind": {
-    "speed": 2.65,
-    "deg": 197,
-    "gust": 4.84
-  },
-  "clouds": {
-    "all": 53
-  },
-  "dt": 1646542386,
-  "sys": {
-    "type": 1,
-    "id": 9609,
-    "country": "CN",
-    "sunrise": 1646520066,
-    "sunset": 1646561447
-  },
-  "timezone": 28800,
-  "id": 1816670,
-  "name": "北京市",
-  "cod": 200
-};
-
